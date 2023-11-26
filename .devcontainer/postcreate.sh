7acab6b59f71e3c1a3cb1c36103d5bf74a85cb99
= <=>
2023-11-26 16:59:06 +0000

    added global git config in postcreate.sh

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,3 +1,6 @@
 pip3 install --upgrade pip
 pip3 install --user -r ./requirements.txt
 pip3 install -e . 
+git config --global user.name = "Ralph Vogl"
+git config --global user.email = "neutro2@outlook.de"
+
