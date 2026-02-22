ccbca50c5571af47e1ab6d26c1157e1cdcd6dbab
Ralph Vogl <neutro2@outlook.de>
2026-02-22 11:53:02 +0100

    neutro2/neural-448-basefunctions-einfuhrung-version

--- templates/python_package/package/__init__.py
+++ templates/python_package/package/__init__.py
@@ -16,16 +16,40 @@
 =============================================================================
 """
 
+# =============================================================================
+# IMPORTS
+# =============================================================================
+from basefunctions.runtime.version import version as _get_version_string
+
 # -------------------------------------------------------------
 # Main package imports
 # -------------------------------------------------------------
 # from <package_name>.core.main_module import MainClass
 # from <package_name>.utils.helpers import helper_function
 
+# =============================================================================
+# VERSION
+# =============================================================================
+__version__ = _get_version_string("<package_name>")
+
+
+def get_version() -> str:
+    """
+    Return the current version of the package.
+
+    Returns
+    -------
+    str
+        Version string (e.g., "1.2.3" or "1.2.3-dev+5")
+    """
+    return __version__
+
+
 # -------------------------------------------------------------
 # EXPORT DEFINITIONS
 # -------------------------------------------------------------
 __all__ = [
+    "get_version",
     # Add your public API exports here
     # "MainClass",
     # "helper_function",
